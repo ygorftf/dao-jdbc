6ce208417dbb3c9e3a5f2651cad4773ae2b5b6d6
Ygor Torres <ygorftf@gmail.com>
2023-10-22 10:52:04 -0300

    Seller entity class

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,13 +1,17 @@
 package Main;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Arrays;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         Department dp = new Department(1,"Eletronics");
-        System.out.print(dp);
+        Seller seller = new Seller(
+                21,"ygor","ygor@gmail.com", new Date(),3000.00, dp);
+        System.out.println(seller);
     }
 }
