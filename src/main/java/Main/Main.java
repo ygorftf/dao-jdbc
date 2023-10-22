63adc6f4ec8aa382095585e01eae0fbee9e19d79
Ygor Torres <ygorftf@gmail.com>
2023-10-22 08:01:21 -0300

    Department entity class

---
+++ src/main/java/Main/Main.java
@@ -0,0 +1,13 @@
+package Main;
+
+import model.entities.Department;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department dp = new Department(1,"Eletronics");
+        System.out.print(dp);
+    }
+}
