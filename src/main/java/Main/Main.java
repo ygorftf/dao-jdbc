be138d23cbc16fc0575d2cedf1a352b68f9c0c9c
Ygor Torres <ygorftf@gmail.com>
2023-10-23 05:59:52 -0300

    findById implementation

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -11,11 +11,9 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
 
-        Department dp = new Department(1,"Eletronics");
-        Seller seller = new Seller(
-                21,"ygor","ygor@gmail.com", new Date(),3000.00, dp);
-        
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
